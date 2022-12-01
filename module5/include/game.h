96e819a72c45926dfb0d1660e1eda3653098c4e8
evan kirkiles <kirkilese@gmail.com>
2022-12-01 16:52:55 -0500

    debugging ESP NOW bug.

--- module5/include/game.h
+++ module5/include/game.h
@@ -1,36 +1,36 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <vector>
+
 #include "client_espnow.h"
 #include "client_peripherals.h"
 #include "config.h"
 #include "state_interface.h"
-#include <vector>
 
 namespace cs334 {
 
 class Game {
-public:
-  Game();      // builds the game instance with default values.
-  ~Game();     // destructor frees all of the allocated states
-  void loop(); // iterates through all states a single time.
+ public:
+  Game();       // builds the game instance with default values.
+  ~Game();      // destructor frees all of the allocated states
+  void loop();  // iterates through all states a single time.
 
-public:
+ public:
   // the player running this instance of the game
   player_state_t m_player{
-      .is_seeker = false, .is_authoritative = false, .health = 0};
+      .is_seeker = false, .is_authoritative = true, .health = 0};
 
   // ESP-NOW client for connecting to other players
   // Peripherals client for receiving / sending peripheral states
-  Client::ESPNOW *m_esp_client;
   Client::Peripherals *m_peripherals_client;
   int num_initial_players = 0;
 
-private:
+ private:
   // states of the game to iterate through
   std::vector<GameState *> m_states;
 };
 
-}; // namespace cs334
+};  // namespace cs334
 
 #endif /* GAME_H */
